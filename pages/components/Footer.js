24de9c5c415ccf2dce458ca65b6b9b941499171e
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-06-20 14:15:00 -0500

    basic footer/flex box changes

--- pages/components/Footer.js
+++ pages/components/Footer.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear();
+  return (
+    <footer className='w-full flex justify-center items-center h-20 bg-light-navy text-navy '>
+      <p>Copyright &copy; {year} Digital Journal</p>
+    </footer>
+  );
 };
 
 export default Footer;
