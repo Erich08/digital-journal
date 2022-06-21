a94ccf6ba820d6c7d4ec95f1e1739a765c68920e
Erich Hartnauer <erich.hartnauer@gmail.com>
2022-06-20 21:04:07 -0500

    entry component added with firebase and mui

---
+++ pages/firebaseConfig.js
@@ -0,0 +1,16 @@
+import { getFirestore } from 'firebase/firestore';
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAFMz2BgtwXzOfoutjtj0FVrVGdeC03gLM',
+  authDomain: 'evernote-clone-94a89.firebaseapp.com',
+  projectId: 'evernote-clone-94a89',
+  storageBucket: 'evernote-clone-94a89.appspot.com',
+  messagingSenderId: '294175451710',
+  appId: '1:294175451710:web:0f1e9dec536c211b813f6f',
+  measurementId: 'G-QL5KC5JQR6',
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
